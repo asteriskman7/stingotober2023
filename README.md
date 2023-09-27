34eb1d59d0507d8adcab51c739572e5fa86d2d7a
asteriskman <asteriskman7@users.noreply.github.com>
2023-09-27 11:48:35 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Stingotober 2023
+https://asteriskman7.github.io/stingotober2023
+
+javascript canvas sketches based on the [Stingotober 2023](https://www.instagram.com/p/CwqHARLOdTY) prompts
---
+++ sketch0.js
@@ -0,0 +1,17 @@
+'use strict';
+
+window.sketchClass = class extends Sketch {
+  desc = '-';
+
+  draw(ctx, width, height, t) {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, width, height);
+    ctx.font = '20px Grandstander';
+    ctx.fillStyle = 'white';
+    ctx.fillText('Please select a sketch from the list above', 10, 30);
+
+  }
+}
+
+window.sketchNumber = document.currentScript.dataset.index;
+app.sketches[window.sketchNumber] = new window.sketchClass();
